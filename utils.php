b061db365efb7f6cb0ed6e5eb890e362fcd84b3f
Branzpir <97573390+Branzpir@users.noreply.github.com>
2022-01-16 15:36:39 +0800

    Add files via upload

---
+++ utils.php
@@ -0,0 +1,80 @@
+<?php
+	require_once 'config.php';
+
+	function connect()
+	{
+		$C = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+		if($C->connect_error)
+		{
+			return false; 
+		}
+		return $C;
+	}
+
+	function sqlSelect($C, $query, $format = false, ...$vars)
+	{
+		$stmt = $C->prepare($query);
+		if($format)
+		{
+			$stmt->bind_param($format, ...$vars);
+		}
+		if($stmt->execute())
+		{
+			$res = $stmt->get_result();
+			$stmt->close();
+			return $res;
+		}
+		$stmt->close();
+		return false;
+	}
+
+	function sqlInsert($C, $query, $format = false, ...$vars)
+	{
+		$stmt = $C->prepare($query);
+		if($format)
+		{
+			$stmt->bind_param($format, ...$vars);
+		}
+		if($stmt->execute())
+		{
+			$id = $stmt->insert_id;
+			$stmt->close();
+			return $id;
+		}
+		$stmt->close();
+		return -1;
+	}
+
+	function createToken()
+	{
+		
+		$seed = urlSafeEncode(random_bytes(8));
+		$t = time();
+		$hash = urlSafeEncode(hash_hmac('sha256', session_id() . $seed . $t, CSRF_TOKEN_SECRET, true));
+		return urlSafeEncode($hash . '|' . $seed . '|' . $t);
+	}
+
+	function validateToken($token)
+	{
+		$parts = explode('|', urlSafeDecode($token));
+		if(count($parts) === 3)
+		{
+			$hash = hash_hmac('sha256', session_id() . $parts[1] . $parts[2], CSRF_TOKEN_SECRET, true);
+			if(hash_equals($hash, urlSafeDecode($parts[0])))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function urlSafeEncode($m)
+	{
+		return rtrim(strtr(base64_encode($m), '+/', '-_'), '=');
+	}
+
+	function urlSafeDecode($m)
+	{
+		return base64_decode(strtr($m, '-_', '+/'));
+	}
+?>
