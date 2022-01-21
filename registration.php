65a3c0ee2e66870ffc5c76809370ed62c004ebc0
Branzpir <97573390+Branzpir@users.noreply.github.com>
2022-01-21 10:25:18 +0800

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,5 +1,5 @@
 <?php
-	session_start();
+	
 	require_once 'utils.php';
 		
 	$errors = [];
@@ -22,7 +22,55 @@
 		{
 			$token_time = $_SESSION['csrf_token_time'];
 			if($token_time + $max_time >= time())
-			{}
+			{	
+				if(!isset($_POST['username']) || strlen($_POST['username']) > 45 || !preg_match('/^[a-zA-Z- ]+$/', $_POST['username'])) {
+					$errors[] = 1;
+				}
+				if(!isset($_POST['email']) || strlen($_POST['email']) > 45 || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+					$errors[] = 2;
+				}
+				else if(!checkdnsrr(substr($_POST['email'], strpos($_POST['email'], '@') + 1), 'MX')) {
+					$errors[] = 3;
+				}
+				if(!isset($_POST['password']) || !preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\~?!@#\$%\^&\*])(?=.{8,})/', $_POST['password'])) {
+					$errors[] = 4;
+				}
+				else if(!isset($_POST['confirm-password']) || $_POST['confirm-password'] !== $_POST['password']) {
+					$errors[] = 5;
+				}
+			
+				//if($errors == 0) {
+					//if($_POST['csrf_token'] == $_SESSION['csrf_token']) {
+					//CONNECT TO DATABASE
+				$C = connect();
+				if($C) {
+					//CHECK IF USER ALREADY EXISTS
+					$res = sqlSelect($C, 'SELECT id FROM accounts WHERE email=?', 's', $_POST['email']);
+					if($res && $res->num_rows === 0) {
+						//CREATE THE ACCOUNT
+						$hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+						$id = sqlInsert($C, 'INSERT INTO accounts VALUES (NULL, ?, ?, ?, 0)', 'sss', $_POST['username'], $_POST['email'], '$hash');
+						if($id !== -1) {
+							$errors[] = 0;
+						}
+						else {
+							//FAILED TO INSERT INTO DATABASE
+							$errors[] = 6;
+						}
+						$res->free_result();
+					}
+					else {
+						//EMAIL ALREADY EXISTS
+						$errors[] = 7;
+					}
+				}
+				else {
+					//FAILED TO CONNECT TO DATABASE
+					$errors[] = 8;
+				}		
+		
+			echo json_encode($errors);
+			}
 			else
 			{
 				unset($_SESSION['csrf_token']);
@@ -34,60 +82,6 @@
 	$token = md5(uniqid(rand(), true));
 	$_SESSION['csrf_token'] = $token;
 	$_SESSION['csrf_token_time'] = time();
-
-	if(!isset($_POST['username']) || strlen($_POST['username']) > 45 || !preg_match('/^[a-zA-Z- ]+$/', $_POST['username'])) {
-		$errors[] = 1;
-	}
-	if(!isset($_POST['email']) || strlen($_POST['email']) > 45 || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-		$errors[] = 2;
-	}
-	else if(!checkdnsrr(substr($_POST['email'], strpos($_POST['email'], '@') + 1), 'MX')) {
-		$errors[] = 3;
-	}
-	if(!isset($_POST['password']) || !preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\~?!@#\$%\^&\*])(?=.{8,})/', $_POST['password'])) {
-		$errors[] = 4;
-	}
-	else if(!isset($_POST['confirm-password']) || $_POST['confirm-password'] !== $_POST['password']) {
-		$errors[] = 5;
-	}
-
-	if($errors == 0) {
-		//if($_POST['csrf_token'] == $_SESSION['csrf_token']) {
-		//CONNECT TO DATABASE
-		$C = connect();
-		if($C) {
-			//CHECK IF USER ALREADY EXISTS
-			$res = sqlSelect($C, 'SELECT id FROM accounts WHERE email=?', 's', $_POST['email']);
-			if($res && $res->num_rows === 0) {
-				//CREATE THE ACCOUNT
-				$hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
-				$id = sqlInsert($C, 'INSERT INTO accounts VALUES (NULL, ?, ?, ?, 0)', 'sss', $_POST['username'], $_POST['email'], '$hash');
-				if($id !== -1) {
-					$errors[] = 0;
-				}
-				else {
-					//FAILED TO INSERT INTO DATABASE
-					$errors[] = 6;
-				}
-				//$res->free_result();
-			}
-			else {
-				//EMAIL ALREADY EXISTS
-				$errors[] = 7;
-			}
-		}
-		else {
-			//FAILED TO CONNECT TO DATABASE
-			$errors[] = 8;
-		}		
-		/*}
-		else {
-			//Invalid CSRF Token
-			$errors[] = 9;
-		}*/
-	}
-
-	echo json_encode($errors);
 
 ?>
 <html>
