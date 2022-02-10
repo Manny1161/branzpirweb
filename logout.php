9cf7bea7c7a9f2c4700a2744a7d6b39c894909ab
Branzpir <97573390+Branzpir@users.noreply.github.com>
2022-02-10 10:23:40 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,14 +1,8 @@
 <?php
     require_once 'utils.php';
-    if(isset($_POST['csrf_token'] == $_SESSION['csrf_token']))
-    {
-        session_destroy();
-        echo 0;
-    }
-    else
-    {
-        echo 1;
-    }
+    session_destroy();
+    $_SESSION['loggedout'] == true;
+    header('location:index.php')
     
 
 ?>
