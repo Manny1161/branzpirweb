64482d2c34b13bb68f972132910e2c2dd711d02d
Branzpir <97573390+Branzpir@users.noreply.github.com>
2022-03-10 10:24:35 +0800

    Update showcase.php

--- showcase.php
+++ showcase.php
@@ -1,49 +1,56 @@
-<!--?php
+<style>
+  .message{position:relative; left:360px; color:red}
+</style>
+
+<?php
+error_reporting(0);
+
 $target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . basename($_FILES["image"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+if(isset($_POST["upload"])) {
+  $check = getimagesize($_FILES["image"]["tmp_name"]);
   if($check !== false) {
     echo "File is an image - " . $check["mime"] . ".";
     $uploadOk = 1;
   } else {
-    echo "File is not an image.";
+    echo '<p class="message">File is not an image</p>';
     $uploadOk = 0;
   }
-}
-// Check if file already exists
-if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
-  $uploadOk = 0;
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
-  $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-  echo "Sorry, only JPG, JPEG & PNG files are allowed.";
-  $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  // Check if file already exists
+  if (file_exists($target_file)) {
+    echo '<p class="message">Sorry, file already exists</p>';
+    $uploadOk = 0;
+  }
+  // Check file size
+  if ($_FILES["image"]["size"] > 500000) {
+    echo '<p class="message">Sorry, your file is too large</p>';
+    $uploadOk = 0;
+  }
+  // Allow certain file formats
+  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+    echo '<p class="message">Sorry, only JPG, JPEG & PNG files are allowed.</p>';
+    $uploadOk = 0;
+  }
+  // Check if $uploadOk is set to 0 by an error
+  if ($uploadOk == 0) {
+    echo '<p class="message">Sorry, your file was not uploaded</p>';
+  // if everything is ok, try to upload file
   } else {
-    echo "Sorry, there was an error uploading your file.";
+    if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+      echo "The file ". htmlspecialchars( basename( $_FILES["image"]["name"])). " has been uploaded.";
+    } else {
+      echo '<p class="message">Sorry, there was an error uploading your file.';
+    }
   }
 }
-?>-->
+
+?>
 
 <?php
-error_reporting(0);
+
 require_once 'utils.php';
 $msg = '';
 if(isset($_POST['upload']))
@@ -57,7 +64,12 @@ if(isset($_POST['upload']))
     $res = sqlInsert($C, "INSERT INTO images (filename) VALUES ('$fname')");
     move_uploaded_file($tempname, 'uploads/'.$filename);
     mysqli_query($C, $res);
-   
+    $id = 8;
+    $res1 = sqlSelect($C, "SELECT username FROM professionals WHERE id=?", $id);
+    if($res1 && $res1->num_rows == 1)
+    {
+      $user = $res1->fetch_assoc();
+    }
   }
   else
   {
@@ -87,6 +99,9 @@ $submitbutton=$_POST['submit'];
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
     <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-highway.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
     
 </head>
 <style>
@@ -107,8 +122,13 @@ $submitbutton=$_POST['submit'];
     -webkit-transition: width:0.4s ease-in-out;}}
     input[type=text]:focus{width:50%}
 
+    .profUpld{float:right; margin-top:-50px; border-radius:20px; margin-right:5px; border:none; color:black; background-color:white;}
     .uploadbtn{float:right; margin-top:-30px;}
     .button{float:right; margin-top:-30px;}
+    .fileinpt{float:right; margin-top:-30px;}
+    .nfile{float:right; margin-top:-25px; border-radius:20px; border:none; color:black; background-color:white; cursor:pointer}
+    .fileUpld{width:4%;float:right; margin-top:-25px; cursor:pointer}
+    
 </style>
 <body>
 <nav class="w3-sidebar w3-highway-red w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:300px;font-weight:bold;" id="mySidebar"><br>
@@ -123,15 +143,13 @@ $submitbutton=$_POST['submit'];
         <a href="professionals.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Professionals</a> 
         <a href="contact.html" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Contact</a>
         <a href="youandbranzpir.html" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">You and Branzpir</a>
-        <a href='login.php' onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Login</a>
-        <a href='registration.php' onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Register</a>
     </div>
 </nav>
 
 <!--- TOP MENU ON SMALL SCREENS -->
 <header class="w3-container w3-top w3-hide-large w3-highway-red w3-xlarge w3-padding">
     <a href="javascript:void(0)" class="w3-button w3-highway-red w3-margin-right" onclick="w3_open()">☰</a>
-    <span><a href='index.php' style='text-decoration:none'>branzpir</a></span>
+    <span><a href='index.php' style='text-decoration:white'>branzpir</a></span>
 </header>
 
 <!--- OVERLAY EFFECT WHEN OPENING SIDEBAR ON SMALL SCREENS -->
@@ -144,9 +162,14 @@ $submitbutton=$_POST['submit'];
         <input class='smallSearch' type="text" name="search" placeholder="Search"/>
         <input type="submit" name="submit" value="Search"/></form>
         <?php if (($_SESSION['loggedin'] == true) && isset($_SESSION['profID'])) : ?>
-        <form method='POST' action='showcase.php' enctype='multipart/form-data'>
+        <!--form method='POST' action='showcase.php' enctype='multipart/form-data'>
+        <input class='fileinpt' type='text' name='fileinpt' value=''/>
         <input class ='button' type='file' name='image' value='' accept='image/*'/>
-        <input class='uploadbtn' type='submit' name='upload' value='Upload'/></form>
+        <input class='uploadbtn' type='submit' name='upload' value='Upload'/>
+        <input class='fileinpt' type='text' name='fileinpt' value=''/>
+        </form-->
+        <form method='POST' action=''><input class='nfile' type='button' value='Upload'></form>
+        <img class='fileUpld' src='568717.png' onclick="location.href='uploadSpaces.php';">
         <?php endif ?>
         <h1 class="w3-jumbo"><b>Be Visually Inspired</b></h1>
         <h1 class="w3-xxxlarge"><b>Showcase.</b></h1>
@@ -177,7 +200,7 @@ if ($countsearchterms == 1)
 	{
 	$array[] += "$file";
   
-	 echo "<img style='width: 48%; margin:8px; cursor:pointer;'  src='/uploads/$fileoriginal'>";
+	echo "<img style='width: 48%; height:533px; margin:8px; cursor:pointer;'  src='/uploads/$fileoriginal'>";
 }
 
     }
@@ -316,14 +339,15 @@ echo "No results for this search entered";
 
     <div class="w3-row-padding">
         <div class="w3-half">
-            <img src="uploads/a-frame-signs-2.jpg" style="width:100%" onclick="onClick(this)" alt="Concrete meets bricks">
+            <img src="uploads/a-frame-signs-2.jpg" style="width:100%" onclick="onClick(this)" alt="Concrete meets bricks<br><?php echo $_SESSION['profName'] ?>">
+            <span><p><?php echo $_SESSION['profName']?><br>some other text</p></span>
             <img src="uploads/bar-lightbox-signage-letters-retail-restaurant.jpg" style="width:100%" onclick="onClick(this)" alt="Kitchen">
             <img src="uploads/large-lightbox-signage-commercial.jpeg" style="width:100%"  onclick="onClick(this)" alt="0">
             <img src="uploads/Outdoor-Signage-Foundation-Academy-Orlando-Fl.jpg" style="width:100%" onclick="onClick(this)" alt="2">
             <img src="uploads/overhead-signage-aluminium-letter-engraving.jpg" style="width:100%" onclick="onClick(this)" alt="Light, white and tight scandanavian design">
             <img src="uploads/signage-lightbox-overhead.jpg" style="width:100%" onclick="onClick(this)" alt="White walls with designer chairs">
             <img src="uploads/small-signage-pvc-sheet-directions.jpeg" style="width:100%" onclick="onClick(this)" alt="LED lightbox letter sign">
-            <img src="uploads/Stud-Mount-Sign-standoffs.jpg" style="width:100%" onclick="onClick(this)" alt="Overhead graphic sign"> 
+            
         </div>
         <div class="w3-half">
             <img src="uploads/unique-wood-glass-standoff-engraving.jpg" style="width:100%" onclick="onClick(this)" alt="Windows for the atrium">
@@ -333,8 +357,9 @@ echo "No results for this search entered";
             <img src="uploads/car-sign-design-graphic-design.jpg" style="width:100%" onclick="onClick(this)" alt="Bedroom and office in one space">
             <img src="uploads/ecoflex-base-aluminium-sheet-outdoor-function.jpg" style="width:100%" onclick="onClick(this)" alt="Scandanavian design">
             <img src ="uploads/3d-restaurant-signage-1.jpg" style="width:100%" onclick="onClick(this)" alt="Letter mount sign">
-            <img src="uploads/Picture-032.jpg" style="width:100%" onclick="onClick(this)" alt="aluminium sheet sign"> 
-        </div>
+            <img src="uploads/outdoor-sign-wire-stake-estate-directions.jpg" style="width:100%" onclick="onClick(this)" alt="aluminium sheet sign"> 
+            <img src="uploads/Stud-Mount-Sign-standoffs.jpg" style="width:100%" onclick="onClick(this)" alt="Overhead graphic sign"> 
+          </div>
     </div>
 
     <div id="modal01" class="w3-modal w3-black" style="padding-top:0" onclick="this.style.display='none'">
@@ -346,23 +371,12 @@ echo "No results for this search entered";
     </div>
 
 <div class="w3-light-grey w3-container w3-padding-32" style="margin-top:75px;padding-right:58px; width:110%; margin-left:-90px;">
-<p class="w3-right">Powered by <a href="https://eurotechdisplays.com.au/" title="Eurotech" target="_blank" class="w3-hover-opacity" style='text-decoration:none'>Eurotech</a></p>
+<p class="w3-right">Powered by <a href="https://eurotechdisplays.com.au/" title="Eurotech" target="_blank" class="w3-hover-opacity" style='text-decoration:none'>Eurotech<?php echo $user['username'] ?></a></p>
 </div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src='index.js'></script>
 <script>
-function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
-}
-function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
-}
-function onClick(element) {
-    document.getElementById("img01").src = element.src;
-    document.getElementById("modal01").style.display = "block";
-    var captionText = document.getElementById("caption");
-    captionText.innerHTML = element.alt;
-}
+$('.message').delay(3000).fadeOut('slow', function() { $(this).remove(); });
 </script>
 </body>
 </html>
