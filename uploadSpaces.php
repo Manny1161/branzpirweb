36560379f0a383cc8e98e6009a41ab54004f51a3
Branzpir <97573390+Branzpir@users.noreply.github.com>
2022-03-10 10:25:23 +0800

    Add files via upload

---
+++ uploadSpaces.php
@@ -0,0 +1,300 @@
+<style>
+  .message{position:relative; left:360px; color:red; margin-top:50px;}
+</style>
+
+<?php
+error_reporting(0);
+
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["image"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+function findexts($filename)
+{
+    $filename = strtolower($filename);
+    $exts = split("[/\\.]", $filename);
+    $n = count($exts)-1;
+    $exts = $exts[$n];
+    return $exts;
+}
+// Check if image file is a actual image or fake image
+if(isset($_POST["upload"])) {
+  $check = getimagesize($_FILES["image"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo '<p class="message">File is not an image</p>';
+    $uploadOk = 0;
+  }
+  // Check if file already exists
+  if (file_exists($target_file)) {
+    echo '<p class="message">Sorry, file already exists</p>';
+    $uploadOk = 0;
+  }
+  // Check file size
+  if ($_FILES["image"]["size"] > 500000) {
+    echo '<p class="message">Sorry, your file is too large</p>';
+    $uploadOk = 0;
+  }
+  // Allow certain file formats
+  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+    echo '<p class="message">Sorry, only JPG, JPEG & PNG files are allowed.</p>';
+    $uploadOk = 0;
+  }
+  // Check if $uploadOk is set to 0 by an error
+  if ($uploadOk == 0) {
+    echo '<p class="message">Sorry, your file was not uploaded</p>';
+  // if everything is ok, try to upload file
+  } else {
+    /*$ext = findexts($_FILES['image']['tmp_name']); 
+    $fname = $_POST['desc'] . '.';*/
+    if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+      echo "The file ". htmlspecialchars( basename( $_FILES["image"]["name"])). " has been uploaded.";
+    } else {
+      echo '<p class="message">Sorry, there was an error uploading your file.';
+    }
+  }
+}
+
+?>
+
+<?php
+
+require_once 'utils.php';
+/*$msg = '';
+if(isset($_POST['upload']))
+{
+  $fname = $_FILES['image']['name'];
+  $tempname = $_FILES['image']['tmp_name'];
+  $folder = 'uploads/'.($fname);
+  if($C = connect())
+  {
+    move_uploaded_file($tempname, $folder);
+    $res = sqlInsert($C, "INSERT INTO images (filename) VALUES ('$fname')");
+    move_uploaded_file($tempname, 'uploads/'.$filename);
+    mysqli_query($C, $res);
+    $id = 8;
+    $res1 = sqlSelect($C, "SELECT username FROM professionals WHERE id=?", $id);
+    if($res1 && $res1->num_rows == 1)
+    {
+      $user = $res1->fetch_assoc();
+    }
+  }
+  else
+  {
+    echo 'fail';
+  }
+}*/
+
+$search=$_POST['search'];
+$searchoriginal=$search;
+$search=strtolower($search);
+$search=trim($search);
+$search=explode(' ', $search);
+$countsearchterms=count($search);
+$submitbutton=$_POST['submit'];
+?>
+
+<html lang="en">
+<head>
+    <title>Branzpir</title>
+    <link rel='branzpir icon' href='branzpir_favicon.png' type='image/x-icon'>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+    <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-highway.css">
+</head>
+<style>
+    body,h1,h2,h3,h4,h5 {font-family: "Poppins", san-serif}
+    body {font-size:16px;}
+    table{margin:0 auto;}
+    .button{margin-top:70px}
+    .buttontxt{margin-top:70px}
+</style>
+<body>
+<header class="w3-container w3-top w3-hide-small w3-highway-red w3-xlarge w3-padding">
+    <b><span><a href='index.php' style='text-decoration:none'>branzpir</a></span></b>
+</header>
+
+<form method='POST' action='' enctype='multipart/form-data' style='margin-top:80px'>
+    <table border='0' align='center' cellpadding='8'>
+        <tr>
+            <td align='right'>Choose File:</td>
+            <td><input type='file' name='image' value='' accept='image/*'/></td>
+        </tr>
+        <tr>    
+            <td align='right'>Description:</td>
+            <td><input type='text' name='desc' placeholder="Enter Detailed Description Of Image" style='width:300px;' required/></td>
+        </tr>
+        <tr>
+            <td colspan='2' align='center'><input type='submit' name='upload' value='Upload' required/></td> 
+        </tr>
+    </table>
+</form>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+$('.message').delay(3000).fadeOut('slow', function() { $(this).remove(); });
+</script>
+</body>
+
+<?php
+$directory = "uploads/";
+if ($submitbutton){
+if (!empty($searchoriginal)) 
+{
+if (is_dir($directory)){
+
+  if ($open = opendir($directory)){
+if ($countsearchterms == 1)
+{
+    while (($file = readdir($open)) !== false){
+	$fileoriginal= $file;
+	$file= strtolower($file);
+	$position= strpos("$file", ".");
+	$fileextension= substr($file, $position + 1);
+	$fileextension= strtolower($fileextension);
+	
+	
+      if ((strpos("$file",  "$search[0]") !== false) && (($fileextension == "jpg") || ($fileextension == "jpeg") || ($fileextension == "png") || ($fileextension == "bmp")))
+	{
+	$array[] += "$file";
+  
+	echo "<img style='width: 48%; height:533px; margin:8px; cursor:pointer;'  src='/uploads/$fileoriginal'>";
+}
+
+    }
+}
+else if ($countsearchterms == 2) {
+while (($file = readdir($open)) !== false){
+	$fileoriginal= $file;
+	$file= strtolower($file);
+	$position= strpos("$file", ".");
+	$fileextension= substr($file, $position + 1);
+	$fileextension= strtolower($fileextension);
+	
+
+      if (((strpos("$file",  "$search[0]") !== false) && (strpos("$file",  "$search[1]") !== false)) && (($fileextension == "jpg") 
+|| ($fileextension == "jpeg") || ($fileextension == "png") || ($fileextension == "bmp"))) {
+	$array[] += "$file";
+	 echo "<img style='width: 200px;' src='/images/$fileoriginal'>". "<br><br><hr>";
+}
+ 
+	
+    }
+    
+
+}
+
+else if ($countsearchterms == 3) {
+while (($file = readdir($open)) !== false){
+	$fileoriginal= $file;
+	$file= strtolower($file);
+	$position= strpos("$file", ".");
+	$fileextension= substr($file, $position + 1);
+	$fileextension= strtolower($fileextension);
+	
+
+      if (((strpos("$file",  "$search[0]") !== false) && (strpos("$file",  "$search[1]") !== false) && (strpos("$file",  "$search[2]") !== false)) 
+&& (($fileextension == "jpg") || ($fileextension == "jpeg") || ($fileextension == "png") || ($fileextension == "bmp")))
+	{
+	$array[] += "$file";
+	 echo "<img style='width: 200px;' src='/images/$fileoriginal'>". "<br><br><hr>";
+}
+ 
+	
+    }
+    
+
+}
+
+else if ($countsearchterms == 4) {
+while (($file = readdir($open)) !== false){
+	$fileoriginal= $file;
+	$file= strtolower($file);
+	$position= strpos("$file", ".");
+	$fileextension= substr($file, $position + 1);
+	$fileextension= strtolower($fileextension);
+	
+
+      if (((strpos("$file",  "$search[0]") !== false) && (strpos("$file",  "$search[1]") !== false) && (strpos("$file",  "$search[2]") !== false)&& (strpos("$file",  "$search[3]") !== false))
+&& (($fileextension == "jpg") || ($fileextension == "jpeg") || ($fileextension == "png") || ($fileextension == "bmp")))
+	{
+	$array[] += "$file";
+	echo "<img style='width: 200px;' src='/uploads/$fileoriginal'>". "<br><br><hr>";
+}
+ 
+	
+    }
+    
+
+}
+
+else if ($countsearchterms == 5) {
+while (($file = readdir($open)) !== false){
+	$fileoriginal= $file;
+	$file= strtolower($file);
+	$position= strpos("$file", ".");
+	$fileextension= substr($file, $position + 1);
+	$fileextension= strtolower($fileextension);
+	
+
+      if (((strpos("$file",  "$search[0]") !== false) && (strpos("$file",  "$search[1]") !== false) && (strpos("$file",  "$search[2]") !== false)&& (strpos("$file",  "$search[3]") !== false)
+&& (strpos("$file",  "$search[4]") !== false)) && (($fileextension == "jpg") || ($fileextension == "jpeg") || ($fileextension == "png") || ($fileextension == "bmp")))
+	{
+	$array[] += "$file";
+	echo "<img style='width: 200px;' src='/uploads/$fileoriginal'>". "<br><br><hr>";
+}
+}  
+}
+else if ($countsearchterms == 6) {
+while (($file = readdir($open)) !== false){
+	$fileoriginal= $file;
+	$file= strtolower($file);
+	$position= strpos("$file", ".");
+	$fileextension= substr($file, $position + 1);
+	$fileextension= strtolower($fileextension);
+	
+
+      if (((strpos("$file",  "$search[0]") !== false) && (strpos("$file",  "$search[1]") !== false) && (strpos("$file",  "$search[2]") !== false)&& (strpos("$file",  "$search[3]") !== false)
+&& (strpos("$file",  "$search[4]") !== false) && (strpos("$file",  "$search[5]") !== false)) && (($fileextension == "jpg") || ($fileextension == "jpeg") 
+|| ($fileextension == "png") || ($fileextension == "bmp")))
+	{
+	$array[] += "$file";
+	echo "<img style='width: 200px;' src='/uploads/$fileoriginal'>". "<br><br><hr>";
+}
+}  
+}
+else if ($countsearchterms == 7) {
+while (($file = readdir($open)) !== false){
+	$fileoriginal= $file;
+	$file= strtolower($file);
+	$position= strpos("$file", ".");
+	$fileextension= substr($file, $position + 1);
+	$fileextension= strtolower($fileextension);
+
+      if (((strpos("$file",  "$search[0]") !== false) && (strpos("$file",  "$search[1]") !== false) && (strpos("$file",  "$search[2]") !== false)&& (strpos("$file",  "$search[3]") !== false)
+&& (strpos("$file",  "$search[4]") !== false) && (strpos("$file",  "$search[5]") !== false) && (strpos("$file",  "$search[6]") !== false))
+&& (($fileextension == "jpg") || ($fileextension == "jpeg") || ($fileextension == "png") || ($fileextension == "bmp")))
+	{
+	$array[] += "$file";
+	echo "<img style='width: 200px;' src='/uploads/$fileoriginal'>". "<br><br><hr>";
+}
+}  
+}
+closedir($open);
+    }
+
+  }//while loop
+
+$arraycount= count($array);
+
+if ($arraycount == 0)
+{
+echo "No results for this search entered";
+}
+}
+}
+?>
+</html>
