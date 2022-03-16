6cddbcddaf17b836aa3299d8a661b643a25a3bff
Branzpir <97573390+Branzpir@users.noreply.github.com>
2022-03-16 08:56:53 +0800

    Update uploadSpaces.php

--- uploadSpaces.php
+++ uploadSpaces.php
@@ -75,7 +75,7 @@ $submitbutton=$_POST['submit'];
 <head>
     <title>Branzpir</title>
     <link rel='branzpir icon' href='branzpir_favicon.png' type='image/x-icon'>
-    <meta charset="UTF-8"/>
+    <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
