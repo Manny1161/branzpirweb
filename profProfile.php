856763efe9278d0c9522ab0aa40db1a0b3f8d890
Branzpir <97573390+Branzpir@users.noreply.github.com>
2022-04-14 10:34:17 +0800

    Update profProfile.php

--- profProfile.php
+++ profProfile.php
@@ -1,3 +1,5 @@
+
+
 <?php
 require_once 'utils.php';
 error_reporting(0);
@@ -5,7 +7,7 @@ $selected_category = $_GET['category'];
 $_SESSION['cat'] = $_GET['category'];
 $pro = $_SESSION['cat'];
 $C = connect();
-$res = sqlSelect($C, 'SELECT description, category, number, address1, state, postcode, address2 FROM professionals WHERE username=?', 's', $_SESSION['cat']);
+$res = sqlSelect($C, 'SELECT description, category, number, address1, state, postcode, address2, facebook, linkedin, website FROM professionals WHERE username=?', 's', $_SESSION['cat']);
 $img = sqlSelect($C, 'SELECT filename FROM images WHERE username=?', 's', $_SESSION['cat']);
 if($res && $res->num_rows == 1)
 {
@@ -15,8 +17,12 @@ if($res && $res->num_rows == 1)
     $_SESSION['profNum'] = $q['number'];
     $_SESSION['profAddy1'] = $q['address1'];
     $_SESSION['profAddy2'] = $q['address2'];
+    
     $_SESSION['profPost'] = $q['postcode'];
     $_SESSION['profState'] = $q['state'];
+    $_SESSION['profFacebook'] = $q['facebook'];
+    $_SESSION['profLinkedin'] = $q['linkedin'];
+    $_SESSION['profWebsite'] = $q['website'];
 }
 if($img && $img->num_rows==1)
 {
@@ -38,6 +44,77 @@ if(is_dir($directory));
             } 
         }
     }
+}
+if(isset($_POST['subBtn'])){
+    if(!empty($_POST['category'])) {
+        sqlUpdate($C, "UPDATE professionals SET category=? WHERE username=?", 'ss', $_POST['category'], $_SESSION['cat']);
+    }
+    if(!empty($_POST['addy1'])) {
+        sqlUpdate($C, "UPDATE professionals SET address1=? WHERE username=?", 'ss', $_POST['addy1'], $_SESSION['cat']);
+    }
+    if(!empty($_POST['addy2'])) {
+        sqlUpdate($C, "UPDATE professionals SET address2=? WHERE username=?", 'ss', $_POST['addy2'], $_SESSION['cat']);
+    }
+    if(!empty($_POST['state'])) {
+        sqlUpdate($C, "UPDATE professionals SET state=? WHERE username=?", 'ss', $_POST['state'], $_SESSION['cat']);
+    }
+    if(!empty($_POST['post-code'])) { 
+        sqlUpdate($C, "UPDATE professionals SET postcode=? WHERE username=?", 'ss', $_POST['post-code'], $_SESSION['cat']);
+    }
+    if(!empty($_POST['business-desc'])) {
+        sqlUpdate($C, "UPDATE professionals SET description=? WHERE username=?", 'ss', $_POST['business-desc'], $_SESSION['cat']);
+    } 
+    if(!empty($_POST['number'])) {
+        sqlUpdate($C, "UPDATE professionals SET number=? WHERE username=?", 'ss', $_POST['number'], $_SESSION['cat']);
+    }  
+    if(!empty($_POST['facebook'])) {
+        sqlUpdate($C, "UPDATE professionals SET facebook=? WHERE username=?", 'ss', $_POST['facebook'], $_SESSION['cat']);
+    }
+    if(!empty($_POST['linkedin'])) {
+        sqlUpdate($C, "UPDATE professionals SET linkedin=? WHERE username=?", 'ss', $_POST['linkedin'], $_SESSION['cat']);
+    }
+    if(!empty($_POST['weburl'])) {
+        sqlUpdate($C, "UPDATE professionals SET website=? WHERE username=?", 'ss', $_POST['weburl'], $_SESSION['cat']);
+    }
+}
+
+$target_dir = "profileavatars/";
+$target_file = $target_dir . basename($_FILES["image"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["upload"])) {
+  $check = getimagesize($_FILES["image"]["tmp_name"]);
+  $imagedetails = getimagesize($_FILES["image"]["tmp_name"]);
+  $width = $imagedetails[0];
+  $height = $imagedetails[1];
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo '<p class="message">File is not an image</p>';
+    $uploadOk = 0;
+  }
+  // Allow certain file formats
+  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+    echo '<p class="message">Sorry, only JPG, JPEG & PNG files are allowed.</p>';
+    $uploadOk = 0;
+  }
+  // Check if $uploadOk is set to 0 by an error
+  if ($uploadOk == 0) {
+    echo '<p class="message">Sorry, your file was not uploaded</p>';
+  // if everything is ok, try to upload file
+  } else {
+    if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+      $fname = $_FILES['image']['name'];
+      $user = $_SESSION['cat'];
+      $res = sqlInsert($C, "INSERT INTO profileavatar (filename, username) VALUES ('$fname', '$user')");
+      
+    } else {
+      echo '<p class="message">Sorry, there was an error uploading your file.';
+    }
+  }
 }
 ?>
 
@@ -64,10 +141,11 @@ if(is_dir($directory));
     .mainImg{width:100%; height: 490px;padding-top:15px}
     .centered{position:absolute;top:350px;margin-left:155px;width:100%}
     .profImg{ position:absolute;margin-top:-152px;}
+    .camera1{position:absolute; margin-left:115px; width:20%; cursor:pointer}
     .bannerImg{position:relative;}
-    .ficon{width:5%}
-    .licon{width:5%}
-    .wicon{width:5%}
+    .ficon{width:2%}
+    .licon{width:2%}
+    .wicon{width:2%}
 </style>
 <body>
 <nav class="w3-sidebar w3-highway-red w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:300px;font-weight:bold;" id="mySidebar"><br>
@@ -91,21 +169,10 @@ if(is_dir($directory));
 <!--- OVERLAY EFFECT WHEN OPENING SIDEBAR ON SMALL SCREENS -->
 <div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
 
-<?php if($_SESSION['loggedin'] && $_SESSION['profName'] == $_SESSION['cat']) : ?>
-    <button type="button" id="editBtn">Edit Profile</button>
-    <form action='' method='POST'>
-        <input type="text" name="textBox" id="textBox"/>
-        <input type="submit" name="subBtn" value="Save" id="subBtn"/>
-    </form>
-    <?php if(isset($_POST['subBtn'])){
-        sqlUpdate($C, "UPDATE professionals SET category=? WHERE username=?", 'ss', $_POST['textBox'], $_SESSION['cat']);   
-    }?>
-    <script src="index.js"></script>
-<?php endif?>
-</h5>
+
 
 <?php
-    if($proImg=sqlSelect($C, 'SELECT filename FROM images WHERE username=?', $_SESSION['cat']))
+    if($proImg=sqlSelect($C, 'SELECT filename FROM profileavatar WHERE username=?', $_SESSION['cat']))
     {
         if($count=$proImg->num_rows)
         {
@@ -121,7 +188,8 @@ if(is_dir($directory));
     <div class="w3-row-padding">
         <div class="bannerImg"><img src="uploads/home.png" class="mainImg"></div>
         <?php
-            if($pf=sqlSelect($C, 'SELECT filename FROM images WHERE username=?', 's', $_SESSION['cat']))
+            //sqlUpdate($C, 'DELETE FROM profileavatar WHERE username=?', 's', $_SESSION['cat']);
+            if($pf=sqlSelect($C, 'SELECT p1.filename FROM profileavatar p1 INNER JOIN profileavatar p2 ON p1.username=p2.username AND p1.id > p2.id WHERE p2.username=?', 's', $_SESSION['cat']))
             {
                 if($count=$pf->num_rows)
                 {
@@ -130,7 +198,31 @@ if(is_dir($directory));
                     {
         ?>
                         <div class="profImg">
-                            <?php echo "<img style='width:150px; height:150px;'src='uploads/$irow->filename'>" ?>
+                            <?php if($_SESSION['loggedin'] && $_SESSION['profName'] == $_SESSION['cat']) : ?>
+                            <img src="camera.png" class="camera1" onclick="openProfilePicture()">
+                            <div id="openProfilePictureForm" class="w3-modal">
+                                <div class="w3-modal-content w3-card-4 w3-animate-zoom" style="max-width:600px">
+                                    <div class="w3-center"><br>
+                                        <span onclick="closeProfilePicture()" class="w3-button w3-xlarge w3-hover-red w3-display-topright" title="Close Form">&times;</span>
+                                    </div>
+                                    <form method='POST' action='' enctype='multipart/form-data' style='margin-top:80px'>
+                                        <table border='0' align='center' cellpadding='8'>
+                                            <tr>
+                                                <td align='right'>Choose File:</td>
+                                                <td><input type='file' name='image' value='' accept='image/*'/></td>
+                                            </tr>
+                                            <tr>
+                                                <td colspan='2' align='center'><input type='submit' name='upload' value='Upload' required/></td> 
+                                            </tr>
+                                        </table>
+                                    </form>
+                                    <div class="w3-container w3-border-top w3-padding-16">
+                                        <button onclick="closeProfilePicture()" type="button" class="w3-button w3-red">Cancel</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php endif?>
+                            <?php echo "<img style='width:150px; height:150px;'src='profileavatars/$irow->filename'>" ?>
                         </div>
 <?php
                     }
@@ -146,23 +238,110 @@ if(is_dir($directory));
             <h2 style="color:white"><b><?php echo $_SESSION['cat']?></b></h2>
             <div display="flex"><span style="color:white"><?php echo $_SESSION['profAddy1'];?>&nbsp;<?php echo $_SESSION['profAddy2'];?></span></div>
             <div display="flex"><span style="color:white"><?php echo $_SESSION['profState'];?>&nbsp;<?php echo $_SESSION['profPost'];?></span></div>
+            <div display="flex"><span style="color:white"><?php echo $_SESSION['profNum'];?>
         </div>
     </div>
+
+    
 
     <div class="w3-left">
         <div class="w3-container">
             <h5><span style="color:grey"><?php echo $_SESSION['profCat'];?></span></h5>
-            <img class="ficon" src="icons/facebookicon.png"/>
-            <img class="licon" src="icons/linkedincon.png"/>
-            <img class="wicon" src="icons/websiteicon.png"/>
+            <a href="<?php echo $_SESSION['profFacebook']?>"><img class="ficon" src="facebookicon.png"/></a>
+            <a href="<?php echo $_SESSION['profLinkedin']?>"><img class="licon" src="linkedincon.png"/></a>
+            <a href="<?php echo $_SESSION['profWebsite']?>"><img class="wicon" src="websiteicon.png"/></a>
         </div>
     </div>
         
-
-
     <div class="w3-left">
         <div class="w3-container" style="width:400px">
             <div class="desc"><?php echo $_SESSION['profDesc'];?></span></div>
+        </div>
+    </div>
+
+    
+
+    <div class="w3-right">
+        <div class="w3-container">
+        <?php
+            if($_SESSION['loggedin'] && $_SESSION['profName'] == $_SESSION['cat']) : ?>
+            <button type="button" id="editBtn" style="margin-top:-60px" onclick="openEditProfile()">Edit Profile</button>
+            <div id="openEditProfileForm" class="w3-modal">
+                <div class="w3-modal-content w3-card-4 w3-animate-zoom" style="max-width:600px">
+                    <div class="w3-center"><br>
+                        <span onclick="closeEditProfile()" class="w3-button w3-xlarge w3-hover-red w3-display-topright" title="Close Form">&times;</span>
+                    </div>
+                    <form action='' method='POST'>
+                        <table border='0' align='center' cellpadding='8'>
+                            <tr>
+                                <td align='right'>Business Name:</td>
+                                <td><input class='inpt' type='text' placeholder="Enter Username" name='username'/></td>
+                            </tr>
+                            <tr>
+                                <td align='right'>Professional Category:</td>
+                                <td><input class='inpt' type='text' placeholder="e.g. Sign Printing" name='category'/></td>
+                            </tr>
+                            <tr>
+                                <td align='right'>Phone Number:</td>
+                                <td><input class='inpt' type='text' placeholder="For potential clients to reach you" name='number'/></td>
+                            </tr>
+                            <tr>
+                                <td align='right'>Address Line 1:</td>
+                                <td><input class='inpt' type='text'  placeholder='Enter business address' name='addy1'/></td>
+                            </tr>
+                            <tr>
+                                <td align='right'>Address Line 2:</td>
+                                <td><input class='inpt' type='text' name='addy2'/></td>
+                            </tr>
+                            <tr>
+                                <td align='right'>Postcode:</td>
+                                <td><input class='inpt' type='text' placeholder='Enter postcode' name='post-code'/></td>
+                            </tr>
+                            <tr>
+                                <td align='right'>State:</td>
+                                <td required>
+                                    <select name="state">
+                                        <option value="NULL">Select State</option>
+                                        <option value="ACT">Australian Capital Territory</option>
+                                        <option value="NSW">New South Wales</option>
+                                        <option value="NT">Northern Territory</option>
+                                        <option value="QLD">Queensland</option>
+                                        <option value="SA">South Australia</option>
+                                        <option value="TAS">Tasmania</option>
+                                        <option value="VIC">Victoria</option>
+                                        <option value="WA">Western Australia</option>
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td align='right'>Business Description:</td>
+                                <td><textarea type='text' rows='3' cols='50' placeholder='Give a brief description of your business, services you offer etc. ' name='business-desc'></textarea></td>
+                            </tr>
+                            <tr>
+                                <td align='right'>Website URL:</td>
+                                <td><input class='inpt' type='text' placeholder='Add website URL' name='weburl'/></td>
+                            </tr>
+                            <tr>
+                                <td align='right'>LinkedIn:</td>
+                                <td><input class='inpt' type='text' placeholder='Add LinkedIn Profile URL' name='linkedin'/></td>
+                            </tr>
+                            <tr>
+                                <td align='right'>Facebook:</td>
+                                <td><input class='inpt' type='text' placeholder='Add Facebook profile URL' name='facebook'/></td>
+                            </tr>
+                            <tr>
+                                <td colspan='2' align='center'><input type='SUBMIT' name='subBtn' value='Save'/></td>
+                            </tr>
+                        </table>
+                    </form>
+                    <div class="w3-container w3-border-top w3-padding-16">
+                        <button onclick="closeEditProfile()" type="button" class="w3-button w3-red">Cancel</button>
+                    </div>
+                </div>
+                
+            </div>
+            <script src="index.js"></script>
+        <?php endif?>
         </div>
     </div>
 
