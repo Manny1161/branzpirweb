12191a7ff913e11af1d10f72fbf4524f7912492b
Branzpir <97573390+Branzpir@users.noreply.github.com>
2022-04-19 10:39:58 +0800

    Update newUserProfile.php

--- newUserProfile.php
+++ newUserProfile.php
@@ -1,36 +1,168 @@
-﻿
+<?php
+require_once 'utils.php';
+error_reporting(0);
+$selected_category = $_GET['category'];
+$_SESSION['cat'] = $_GET['category'];
+$pro = $_SESSION['cat'];
+$C = connect();
+$res = sqlSelect($C, 'SELECT description, category, number, address1, state, postcode, address2, facebook, linkedin, website FROM professionals WHERE username=?', 's', $_SESSION['cat']);
+$img = sqlSelect($C, 'SELECT filename FROM images WHERE username=?', 's', $_SESSION['cat']);
+if($res && $res->num_rows == 1)
+{
+    $q = $res->fetch_assoc();
+    $_SESSION['profDesc'] = $q['description'];
+    $_SESSION['profCat'] = $q['category'];
+    $_SESSION['profNum'] = $q['number'];
+    $_SESSION['profAddy1'] = $q['address1'];
+    $_SESSION['profAddy2'] = $q['address2'];
+    
+    $_SESSION['profPost'] = $q['postcode'];
+    $_SESSION['profState'] = $q['state'];
+    $_SESSION['profFacebook'] = $q['facebook'];
+    $_SESSION['profLinkedin'] = $q['linkedin'];
+    $_SESSION['profWebsite'] = $q['website'];
+}
+if($img && $img->num_rows==1)
+{
+    $i = $img->fetch_assoc();
+    $_SESSION['profImg'] = $i['filename'];
+}
+
+$directory = "uploads/";
+if(is_dir($directory));
+{
+    if($open = opendir($directory))
+    {
+        $files = scandir($directory);
+        foreach($files as $file)
+        {
+            if($_SESSION['profImg'] == $file)
+            {
+                $fileoriginal = $file;
+            } 
+        }
+    }
+}
+if(isset($_POST['subBtn'])){
+    if(!empty($_POST['category'])) {
+        sqlUpdate($C, "UPDATE professionals SET category=? WHERE username=?", 'ss', $_POST['category'], $_SESSION['cat']);
+    }
+    if(!empty($_POST['addy1'])) {
+        sqlUpdate($C, "UPDATE professionals SET address1=? WHERE username=?", 'ss', $_POST['addy1'], $_SESSION['cat']);
+    }
+    if(!empty($_POST['addy2'])) {
+        sqlUpdate($C, "UPDATE professionals SET address2=? WHERE username=?", 'ss', $_POST['addy2'], $_SESSION['cat']);
+    }
+    if(!empty($_POST['state'])) {
+        sqlUpdate($C, "UPDATE professionals SET state=? WHERE username=?", 'ss', $_POST['state'], $_SESSION['cat']);
+    }
+    if(!empty($_POST['post-code'])) { 
+        sqlUpdate($C, "UPDATE professionals SET postcode=? WHERE username=?", 'ss', $_POST['post-code'], $_SESSION['cat']);
+    }
+    if(!empty($_POST['business-desc'])) {
+        sqlUpdate($C, "UPDATE professionals SET description=? WHERE username=?", 'ss', $_POST['business-desc'], $_SESSION['cat']);
+    } 
+    if(!empty($_POST['number'])) {
+        sqlUpdate($C, "UPDATE professionals SET number=? WHERE username=?", 'ss', $_POST['number'], $_SESSION['cat']);
+    }  
+    if(!empty($_POST['facebook'])) {
+        sqlUpdate($C, "UPDATE professionals SET facebook=? WHERE username=?", 'ss', $_POST['facebook'], $_SESSION['cat']);
+    }
+    if(!empty($_POST['linkedin'])) {
+        sqlUpdate($C, "UPDATE professionals SET linkedin=? WHERE username=?", 'ss', $_POST['linkedin'], $_SESSION['cat']);
+    }
+    if(!empty($_POST['weburl'])) {
+        sqlUpdate($C, "UPDATE professionals SET website=? WHERE username=?", 'ss', $_POST['weburl'], $_SESSION['cat']);
+    }
+}
+
+$target_dir = "profileavatars/";
+$target_file = $target_dir . basename($_FILES["image"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["upload"])) {
+  $check = getimagesize($_FILES["image"]["tmp_name"]);
+  $imagedetails = getimagesize($_FILES["image"]["tmp_name"]);
+  $width = $imagedetails[0];
+  $height = $imagedetails[1];
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo '<p class="message">File is not an image</p>';
+    $uploadOk = 0;
+  }
+  // Allow certain file formats
+  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+    echo '<p class="message">Sorry, only JPG, JPEG & PNG files are allowed.</p>';
+    $uploadOk = 0;
+  }
+  // Check if $uploadOk is set to 0 by an error
+  if ($uploadOk == 0) {
+    echo '<p class="message">Sorry, your file was not uploaded</p>';
+  // if everything is ok, try to upload file
+  } else {
+    if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+      $fname = $_FILES['image']['name'];
+      $user = $_SESSION['cat'];
+      $res = sqlInsert($C, "INSERT INTO profileavatar (filename, username) VALUES ('$fname', '$user')");
+      
+    } else {
+      echo '<p class="message">Sorry, there was an error uploading your file.';
+    }
+  }
+}
+?>
+
+<?php
+    
+    if($pf=sqlSelect($C, 'SELECT p1.filename FROM profileavatar p1 INNER JOIN profileavatar p2 ON p1.username=p2.username AND p1.id > p2.id WHERE p2.username=?', 's', $_SESSION['cat']))
+    {
+        if($count=$pf->num_rows)
+        {
+            while($irow=$pf->fetch_object())
+
+            {
+                $profav = $irow->filename;
+            }
+        }
+    }
+?>
+
 <html lang="en">
 <head>
 <title>branzpir</title>
     <link rel='branzpir icon' href='branzpir_favicon.png' type='image/x-icon'>
     <meta charset="UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1"-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
     <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-highway.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
-
 </head>
 <style>
-.profile-head {
-    transform: translateY(5rem)
-}
-
-.cover {
-    background-image: url('uploads/r35(2).jpg');
-    background-size: cover;
-    background-repeat: no-repeat
-}
-.profImg{height:130px;width:130px;object-fit:cover};
-.brCon{display:none !important};
-body {
-    
-	background-color: #f7f7f7;
-    min-height: 100vh;
-    overflow-x: hidden
-}
+    body,h1,h2,h3,h4,h5 {font-family: "Poppins", san-serif}
+    .profile-head {
+        transform: translateY(5rem)
+    }
+    .cover {
+        background-image: url('uploads/home.png');
+        background-size: cover;
+        background-repeat: no-repeat
+    }
+    .profImg{height:130px;width:130px;object-fit:cover};
+    .dispImg{height:300px;width:300px;object-fit:cover};
+    .brCon{display:none !important; cursor:pointer};
+    body {
+        
+        background-color: #f7f7f7;
+        min-height: 100vh;
+        overflow-x: hidden;
+    }
 </style>
 <body>
 <nav class="w3-sidebar w3-highway-red w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:300px;font-weight:bold;" id="mySidebar"><br>
@@ -46,26 +178,100 @@ body {
         <a href="youandbranzpir.html" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">You and Branzpir</a>
     </div>
 </nav>
-
 <!--- TOP MENU ON SMALL SCREENS -->
 <header class="w3-container w3-top w3-hide-large w3-highway-red w3-xlarge w3-padding">
     <a href="javascript:void(0)" class="w3-button w3-highway-red w3-margin-right" onclick="w3_open()">☰</a>
 </header>
-
 <!--- OVERLAY EFFECT WHEN OPENING SIDEBAR ON SMALL SCREENS -->
 <div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
-
 <div class="row py-5 px-4">
     <div class="col-md-5 mx-auto">
         <!-- Profile widget -->
         <div class="bg-white shadow rounded overflow-hidden">
             <div class="px-4 pt-0 pb-4 cover">
                 <div class="media align-items-end profile-head">
-                    <div class="profile mr-3"><img class="profImg rounded mb-2 img-thumbnail" src="uploads/unnamed.jpg" alt="..." ><a href="#" class="btn btn-outline-dark btn-sm btn-block">Edit profile</a></div>
+                    <div class="profile mr-3"><img class="profImg rounded mb-2 img-thumbnail" src="profileavatars/1624926477201.jpg" alt="..." ><?php if($_SESSION['loggedin'] && $_SESSION['profName'] == $_SESSION['cat']) : ?><button type="button" id="editBtn" onclick="openEditProfile()" class="btn btn-outline-dark btn-sm btn-block">Edit profile</button></div>
                     <div class="media-body mb-5 text-white">
                         <h4 class="mt-0 mb-0">Eurotech Australia</h4>
                         <p class="small mb-4"> <i class="fas fa-map-marker-alt mr-2"></i>72 John ST, Welshpool</p>
                     </div>
+                </div>
+                    <div id="openEditProfileForm" class="w3-modal">
+                        <div class="w3-modal-content w3-card-4 w3-animate-zoom" style="max-width:600px">
+                            <div class="w3-center"><br>
+                                <span onclick="closeEditProfile()" class="w3-button w3-xlarge w3-hover-red w3-display-topright" title="Close Form">&times;</span>
+                            </div>
+                            <form action='' method='POST'>
+                                <table border='0' align='center' cellpadding='8'>
+                                    <tr>
+                                        <td align='right'>Business Name:</td>
+                                        <td><input class='inpt' type='text' placeholder="Enter Username" name='username'/></td>
+                                    </tr>
+                                    <tr>
+                                        <td align='right'>Professional Category:</td>
+                                        <td><input class='inpt' type='text' placeholder="e.g. Sign Printing" name='category'/></td>
+                                    </tr>
+                                    <tr>
+                                        <td align='right'>Phone Number:</td>
+                                        <td><input class='inpt' type='text' placeholder="For potential clients to reach you" name='number'/></td>
+                                    </tr>
+                                    <tr>
+                                        <td align='right'>Address Line 1:</td>
+                                        <td><input class='inpt' type='text'  placeholder='Enter business address' name='addy1'/></td>
+                                    </tr>
+                                    <tr>
+                                        <td align='right'>Address Line 2:</td>
+                                        <td><input class='inpt' type='text' name='addy2'/></td>
+                                    </tr>
+                                    <tr>
+                                        <td align='right'>Postcode:</td>
+                                        <td><input class='inpt' type='text' placeholder='Enter postcode' name='post-code'/></td>
+                                    </tr>
+                                    <tr>
+                                        <td align='right'>State:</td>
+                                        <td required>
+                                            <select name="state">
+                                                <option value="NULL">Select State</option>
+                                                <option value="ACT">Australian Capital Territory</option>
+                                                <option value="NSW">New South Wales</option>
+                                                <option value="NT">Northern Territory</option>
+                                                <option value="QLD">Queensland</option>
+                                                <option value="SA">South Australia</option>
+                                                <option value="TAS">Tasmania</option>
+                                                <option value="VIC">Victoria</option>
+                                                <option value="WA">Western Australia</option>
+                                            </select>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td align='right'>Business Description:</td>
+                                        <td><textarea type='text' rows='3' cols='50' placeholder='Give a brief description of your business, services you offer etc. ' name='business-desc'></textarea></td>
+                                    </tr>
+                                    <tr>
+                                        <td align='right'>Website URL:</td>
+                                        <td><input class='inpt' type='text' placeholder='Add website URL' name='weburl'/></td>
+                                    </tr>
+                                    <tr>
+                                        <td align='right'>LinkedIn:</td>
+                                        <td><input class='inpt' type='text' placeholder='Add LinkedIn Profile URL' name='linkedin'/></td>
+                                    </tr>
+                                    <tr>
+                                        <td align='right'>Facebook:</td>
+                                        <td><input class='inpt' type='text' placeholder='Add Facebook profile URL' name='facebook'/></td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan='2' align='center'><input type='SUBMIT' name='subBtn' value='Save'/></td>
+                                    </tr>
+                                </table>
+                            </form>
+                            <div class="w3-container w3-border-top w3-padding-16">
+                                <button onclick="closeEditProfile()" type="button" class="w3-button w3-red">Cancel</button>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <script src="index.js"></script>
+                    <?php endif?>
                 </div>
             </div>
             <div class="bg-light p-4 d-flex justify-content-end text-center">
@@ -99,7 +305,7 @@ body {
                     <p class="font-italic mb-0">Photographer</p>
                 </div>
             </div>
-			 <div id="openReviewForm" class="brCon px-4 py-3 ">
+			 <div id="openReviewForm" class="brCon px-4 py-3 " style="display:none">
                 <h5 class="mb-0">Reviews</h5>
                 <div class="p-4 rounded shadow-sm bg-light">
                     <p class="font-italic mb-0">Web Developer</p>
@@ -107,7 +313,7 @@ body {
                     <p class="font-italic mb-0">Photographer</p>
                 </div>
             </div>
-			 <div id="openContactForm" class="brCon px-4 py-3 ">
+			 <div id="openContactForm" class="brCon px-4 py-3" style="display:none">
                 <h5 class="mb-0">Contact</h5>
                 <div class="p-4 rounded shadow-sm bg-light">
                     <p class="font-italic mb-0">Web Developer</p>
@@ -120,15 +326,31 @@ body {
                     <h5 class="mb-0">Recent photos</h5><a href="#" class="btn btn-link text-muted">Show all</a>
                 </div>
                 <div class="row">
-                    <div class="col-lg-6 mb-2 pr-lg-1"><img src="https://images.unsplash.com/photo-1469594292607-7bd90f8d3ba4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="" class="img-fluid rounded shadow-sm"></div>
-                    <div class="col-lg-6 mb-2 pl-lg-1"><img src="https://images.unsplash.com/photo-1493571716545-b559a19edd14?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="" class="img-fluid rounded shadow-sm"></div>
-                    <div class="col-lg-6 pr-lg-1 mb-2"><img src="https://images.unsplash.com/photo-1453791052107-5c843da62d97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="" class="img-fluid rounded shadow-sm"></div>
-                    <div class="col-lg-6 pl-lg-1"><img src="https://images.unsplash.com/photo-1475724017904-b712052c192a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="" class="img-fluid rounded shadow-sm"></div>
+                <?php
+                    if($proImg=sqlSelect($C, 'SELECT filename, description FROM images WHERE username=?', 's', $_SESSION['cat']))
+                    {
+                        if($count=$proImg->num_rows)
+                        {
+                            while($imgRow=$proImg->fetch_object())
+                            {
+                ?>
+                                
+                                <div class="col-lg-6 pl-lg-1">
+                                    <?php echo "<img class='img-fluid rounded shadow-sm' src='uploads/$imgRow->filename'>" ?>
+                                </div>
+                            
+                <?php
+                            }
+                        }
+                        $proImg->free();
+                    }    
+
+                ?>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<script src="script.js"></script>
+<script src="index.js"></script>
 </body>
 </html>
